bb91f4ae52a5790ad2ed3f76ff9ea776b8dbf789
James Kalu <ajkalu68@gmail.com>
2022-09-08 12:45:12 -0700

    Use fun puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - print the string
+ *Description: using the main function
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
