0caed37e365b8246bcce51256c39f686aea157cd
James Kalu <ajkalu68@gmail.com>
2022-09-13 14:57:20 -0700

    Print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - Prints all alpahbets in 10 lines
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+
+	for (int i = 0; i < 9; i++)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+	
+		_putchar('\n')
+
+	}
+}
