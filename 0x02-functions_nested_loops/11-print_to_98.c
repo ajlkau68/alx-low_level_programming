27c2d602c3066350bed7a89a71841f5c2bfae664
James Kalu <ajkalu68@gmail.com>
2022-09-13 17:34:26 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+include "main.h"
+/**
+ * print_to_98 - Describes the main function
+ *
+ * Description:
+ * @n: is the parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				_putchar('0' + i);
+			else
+				_putchar('0' + i);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+				_putchar('0' + i);
+			else
+				_putchar ('0' + i);
+		}
+	}
+	_putchar('\n');
+}
+
