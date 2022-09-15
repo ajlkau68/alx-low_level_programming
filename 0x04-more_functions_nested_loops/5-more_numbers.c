ef17418ed7f20b5057882ef3205cc5591eee91ab
James Kalu <ajkalu68@gmail.com>
2022-09-15 12:40:43 -0700

    Print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0, j < 15; j++)
-		{
-			_putchar(j + '0');
-		}
-
-		_putchar('\n');
+		if (j >= 10)
+			_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
 	}
+	_putchar('\n');
 }
